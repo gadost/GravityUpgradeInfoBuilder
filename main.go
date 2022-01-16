00a596ec557c12eff158617ea3d5fd6da5f3dcdc
Max N <nikolaev.makc@gmail.com>
2022-01-16 16:17:17 +0200

    calculate upgrade height

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
     "fmt"
     "io"
     "net/http"
+    "time"
 )
 
 var releaseTag string
+var currentHeight int64
+var blockTime int64
+var apxDate string
 var binariesMap = map[string]string{
     "linux/amd64":   "gravity-linux-amd64",
     "darwin/amd64":  "gravity-darwin-amd64",
@@ -19,20 +23,28 @@ var binariesMap = map[string]string{
 
 func init() {
     flag.StringVar(&releaseTag, "tag", "", "release tag \ne.g  -tag v1.3.0")
+    flag.Int64Var(&currentHeight, "current-height", 0, "current height e.g. 400001")
+    flag.Int64Var(&blockTime, "blocktime", 6300, "current block time in ms e.g. 6300")
+    flag.StringVar(&apxDate, "date", "", "date&time for update e.g. 18 Jan 22 16:00 UTC   (RFC822)")
 }
 
 func main() {
     flag.Parse()
-    if releaseTag == "" {
-        panic("please specify -tag <release tag>")
+    if currentHeight != 0 && blockTime != 0 && apxDate != "" {
+        tsX := parseDate(apxDate).UnixMilli()
+        tsN := time.Now().UTC().UnixMilli()
+        blockX := (tsX-tsN)/(blockTime) + currentHeight
+        fmt.Printf("at %s ~ calculated height %d\n", apxDate, blockX)
     }
-    var binaryDownloadUrl = fmt.Sprintf("https://github.com/Gravity-Bridge/Gravity-Bridge/releases/download/%s/", releaseTag)
-    var binary = map[string]string{}
-    for k, v := range binariesMap {
-        binary[k] = binaryDownloadUrl + v + "?checksum=sha256:" + getSHA256(getFile(binaryDownloadUrl+v))
-    }
+    if len(releaseTag) > 0 {
+        var binaryDownloadUrl = fmt.Sprintf("https://github.com/Gravity-Bridge/Gravity-Bridge/releases/download/%s/", releaseTag)
+        var binary = map[string]string{}
+        for k, v := range binariesMap {
+            binary[k] = binaryDownloadUrl + v + "?checksum=sha256:" + getSHA256(getFile(binaryDownloadUrl+v))
+        }
 
-    buildJSON(binary)
+        buildJSON(binary)
+    }
 }
 
 func getFile(s string) []byte {
@@ -57,4 +69,9 @@ func buildJSON(s map[string]string) {
     res, _ := json.Marshal(x)
     fmt.Println(string(res))
 
+}
+
+func parseDate(s string) time.Time {
+    t, _ := time.Parse(time.RFC822, s)
+    return t
 }
